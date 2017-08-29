e85af7daa5e2e9697cd1da98d005244de84a65d4
Yucos <yu_cos@qq.com>
2017-08-29 18:12:57 +0800

    Update get_hosts.cmd

--- get_hosts.cmd
+++ get_hosts.cmd
@@ -1,9 +1,14 @@
-wget https://raw.githubusercontent.com/Yucos/-hosts/master/hosts -O S:\hosts
-for %%a in (S:\hosts) do if %%~za==0 (echo 空空空空空文件) else (copy /y S:\hosts "%SystemRoot%\System32\drivers\etc\hosts")
+@echo ------------------------------------------
+@echo -                                        -
+@echo -             获取HOSTS文件？            -
+@echo -                                        -
+@echo ------------------------------------------
+rem pause
+rem wget https://raw.githubusercontent.com/racaljk/hosts/master/hosts -x -O C:\hosts
+wget https://raw.githubusercontent.com/Yucos/-hosts/master/hosts -x -O C:\hosts
+for %%a in (C:\hosts) do if %%~za==0 (echo 空空空空空空空的文件) else (copy /y C:\hosts "%SystemRoot%\System32\drivers\etc\hosts")
 ipconfig /flushdns
 @echo ------------------------------
-@echo *                            *
+@echo -                            -
 @echo off
-head -3 C:\WINDOWS\System32\drivers\etc\hosts
-@echo *                            *
-@echo ------------------------------
+head -3 C:\WINDOWS\System32\drivers\etc\hosts |tail -1
